a35dd8e7071278858e0e59df1f25c70b6ab3f89b
Arnab-1473 <119725146+Arnab-1473@users.noreply.github.com>
2023-03-16 12:45:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+	const cleanedStr = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
+	for (let i = 0; i < cleanedStr.length; i++) {
+		if (cleanedStr[i] != cleanedStr[cleanedStr.length-1-i]) {
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
